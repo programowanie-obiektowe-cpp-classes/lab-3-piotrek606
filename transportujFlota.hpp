473d4b0715976da2b08e33cdab9d199f36cff0d5
piotrek606 <150963313+piotrek606@users.noreply.github.com>
2025-11-03 14:24:23 +0100

    flota

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,21 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0)
+        return 0;
+
+    Stocznia     stocznia{};
+    unsigned int suma            = 0;
+    unsigned int liczbaZaglowcow = 0;
+
+    while (suma < towar) {
+        Statek* s = stocznia();          
+        if (dynamic_cast< Zaglowiec* >(s) != nullptr) 
+            ++liczbaZaglowcow;
+
+        suma += s->transportuj(); 
+        delete s;          
+    }
+
+    return liczbaZaglowcow;
 }
